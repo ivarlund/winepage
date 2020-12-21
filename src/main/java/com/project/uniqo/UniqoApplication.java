fc99dd04dc20e3696d0282eb5dcce85a9f430838
ivarlund <49944364+ivarlund@users.noreply.github.com>
2020-12-21 13:24:30 +0100

    initial commit

---
+++ src/main/java/com/project/uniqo/UniqoApplication.java
@@ -0,0 +1,13 @@
+package com.project.uniqo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UniqoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UniqoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/project/uniqo/UniqoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.uniqo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UniqoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
